d62ff287f995a2d6e1120d2abd6110a9068b1450
Ruoyao Wu (RIT Student) <rxw7522@ad.rit.edu>
2018-09-06 13:44:01 -0400

    Nothing

---
+++ TopDownShooting/Assets/2d_Tds/Script/D_CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_CharacterMovement : MonoBehaviour {
+
+    bool IsWalking;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            if (Mathf.Round(Input.GetAxisRaw("LeftTrigger")) > 0 && Mathf.Round(Input.GetAxisRaw("RightTrigger")) > 0)
+                IsWalking = false;
+            else
+                IsWalking = true;
+        }
+
+        else
+            IsWalking = false;
+    }
+
+
+}
