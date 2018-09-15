cf101762252131dbdafcda24f96b25c31834d554
Parth Contractor <parth.contractor94@gmail.com>
2018-09-15 19:44:59 -0400

    Created SpawnPoint Prefab and Script to spwan enemy

---
+++ TopDownShooting/Assets/2d_Tds/Script/enemy_AI/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    private Vector2 startPos;
+
+    public GameObject enemy;
+
+    void Start()
+    {
+        startPos = transform.position;
+
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        GameObject clone = (GameObject)Instantiate(enemy, startPos, Quaternion.identity);
+        clone.transform.parent = transform;
+
+        Invoke("Spawn", 3.0f);
+    }
+}
