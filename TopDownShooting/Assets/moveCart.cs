934ee7991cbd29e8e36b709a19fe97df6f27e055
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2018-09-19 19:19:52 -0400

    ck3

--- TopDownShooting/Assets/moveCart.cs
+++ TopDownShooting/Assets/moveCart.cs
@@ -6,6 +6,8 @@ public class moveCart : MonoBehaviour {
     public GameObject ref1;
     public GameObject ref2;
     int dir = 0;
+    bool p = false;
+    public GameObject collisiona;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,26 +15,21 @@ public class moveCart : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-      
-        if (collision.tag == "Character")
+        if (p)
         {
             if (dir == 0)
             {
                 this.transform.position += new Vector3(0, -.1f, 0);
-                collision.transform.position += new Vector3(0, -.1f, 0);
-                if (this.transform.position.y <= ref2.transform.position.y) {
+                collisiona.transform.position += new Vector3(0, -.1f, 0);
+                if (this.transform.position.y <= ref2.transform.position.y)
+                {
                     dir++;
                 }
             }
             else if (dir == 2)
             {
                 this.transform.position += new Vector3(0, .1f, 0);
-                collision.transform.position += new Vector3(0, .1f, 0);
+                collisiona.transform.position += new Vector3(0, .1f, 0);
                 if (this.transform.position.y >= ref1.transform.position.y)
                 {
                     dir++;
@@ -42,7 +39,7 @@ public class moveCart : MonoBehaviour {
             else if (dir == 1)
             {
                 this.transform.position += new Vector3(.1f, 0, 0);
-                collision.transform.position += new Vector3(.1f, 0, 0);
+                collisiona.transform.position += new Vector3(.1f, 0, 0);
                 if (this.transform.position.x >= ref1.transform.position.x)
                 {
                     dir++;
@@ -52,7 +49,7 @@ public class moveCart : MonoBehaviour {
             else if (dir == 3)
             {
                 this.transform.position -= new Vector3(.1f, 0, 0);
-                collision.transform.position -= new Vector3(.1f, 0, 0);
+                collisiona.transform.position -= new Vector3(.1f, 0, 0);
                 if (this.transform.position.x <= ref2.transform.position.x)
                 {
                     dir = 0;
@@ -61,7 +58,27 @@ public class moveCart : MonoBehaviour {
             }
 
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Character")
+        {
+            p = true;
+        }
         
         
     }
+
+   
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Character")
+        {
+            p = false;
+        }
+    }
+
+
 }
